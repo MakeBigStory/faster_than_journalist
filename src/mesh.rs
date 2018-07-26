8eccb86b3e90f0383ad467fe948315be10b971aa
Michael-LAI JINFENG <cihv2@163.com>
2018-07-26 13:45:35 +0800

    [add] mesh.rs renderbuffer.rs

---
+++ src/mesh.rs
@@ -0,0 +1,43 @@
+enum MeshPrimitive {
+    /// Single points.
+//    Points = GL_POINTS,
+
+    /// Each pair of vertices defines a single line, lines aren't
+    /// connected together.
+    ///
+//    Lines = GL_LINES,
+
+    /// Line strip, last and first vertex are connected together.
+//    LineLoop = GL_LINE_LOOP,
+
+    /// First two vertices define first line segment, each following
+    /// vertex defines another segment.
+//    LineStrip = GL_LINE_STRIP,
+
+    /// Each three vertices define one triangle.
+//    Triangles = GL_TRIANGLES,
+
+    /// First three vertices define first triangle, each following
+    /// vertex defines another triangle.
+//    TriangleStrip = GL_TRIANGLE_STRIP,
+
+    /// First vertex is center, each following vertex is connected to
+    /// previous and center vertex.
+//    TriangleFan = GL_TRIANGLE_FAN,
+}
+
+struct Mesh {
+    primitive: MeshPrimitive,
+}
+
+impl Mesh{
+    fn get_primitive(&self) -> MeshPrimitive {
+        self.primitive
+    }
+
+    fn set_primitive(&self, primitive: MeshPrimitive ){
+        self.primitive = primitive
+    }
+
+
+}
---
+++ src/renderbuffer.rs
@@ -0,0 +1,16 @@
+struct RenderBuffer {}
+
+impl RenderBuffer {
+    /// `MAX_RENDERBUFFER_SIZE`
+    fn max_size() -> i32 {}
+
+    /// `MAX_SAMPLES`
+    fn max_samples() -> i32 {}
+
+    fn wrap(mut self) {}
+
+    fn set_storage(mut self) {}
+    fn set_storage_multi_sample(mut self) {}
+    fn get_label(mut self) {}
+    fn set_label(mut self) {}
+}
