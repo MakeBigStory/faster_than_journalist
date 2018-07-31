578611f9885900c7f1eb9d872ac4159c2e4234a5
lizichen <lizichen@lizichendeMacBook-Pro.local>
2018-07-31 19:01:55 +0800

    [ADD: add android test func]

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,53 @@ static SIMPLE_FRAGMENT: &'static str = "
     }
 ";
 
+const LOG_TAG : &str = "winterfell";
+
+#[macro_use] extern crate log;
+extern crate android_logger;
+use log::Level;
+use android_logger::Filter;
+
+#[cfg(target_os = "android")]
+#[allow(non_snake_case)]
+pub mod android {
+    extern crate jni;
+
+    use self::jni::objects::{JClass, JString};
+    use self::jni::sys::{jlong, jstring};
+    use self::jni::JNIEnv;
+    use super::*;
+
+    #[no_mangle]
+    pub unsafe extern "C" fn Java_com_example_vampire_opengl_Rust_init(
+        env: JNIEnv,
+        _: JClass,
+    ) -> jlong {
+        trace!("{} create program .... ", LOG_TAG);
+
+        let mut program = ShaderProgram::create_shader_program(SIMPLE_VERTEX, SIMPLE_FRAGMENT);
+        program.activate();
+
+        if cfg!(target_os = "android") {
+            android_logger::init_once(Filter::default()
+                .with_min_level(Level::Trace));
+            trace!("{} android logger init .... ", LOG_TAG);
+
+            trace!("{} program {:?} .... ", LOG_TAG, program);
+        }
+
+        0 as jlong
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn Java_com_example_vampire_opengl_Rust_draw(
+        env: JNIEnv,
+        _: JClass,
+        handle: jlong,
+    ) {
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn init_triangle_program() {
     let mut program = ShaderProgram::create_shader_program(SIMPLE_VERTEX, SIMPLE_FRAGMENT);
